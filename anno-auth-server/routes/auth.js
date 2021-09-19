4dd1b0f4f3c8ed08206f6d8fc415f4fc396bfd67
mk-pmb <mk-pmb@users.noreply.github.com>
2021-09-19 18:49:31 +0200

    anno-auth-server: Factor out addAccessPolicyHeaders

--- anno-auth-server/routes/auth.js
+++ anno-auth-server/routes/auth.js
@@ -3,17 +3,13 @@ const cookieParser        = require('cookie-parser')
 const expressSession      = require('express-session')
 const morgan              = require('morgan')
 
+const addAccessPolicyHeaders = require('../src/addAccessPolicyHeaders.js');
+
+
 module.exports = function createAuthRoute({backend, sessionMasterKey}) {
   const app = express.Router()
   app.use(morgan('dev'))
-  app.use((req, resp, next) => {
-    resp.header('Access-Control-Allow-Origin', req.get('Origin'))
-    resp.header('Access-Control-Allow-Headers', 'Content-Type, Prefer, Authorization, X-Anno-Context, X-Anno-Collection, X-Anno-Metadata')
-    resp.header('Access-Control-Allow-Credentials', 'true')
-    resp.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, DELETE, PUT')
-    resp.header('Access-Control-Expose-Headers', 'ETag, Allow, Vary, Link, Content-Type, Location, Content-Location, Prefer')
-    return next()
-  })
+  app.use(addAccessPolicyHeaders);
   app.use(cookieParser())
   app.use(expressSession({
     secret: sessionMasterKey,
